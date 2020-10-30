97e94bf02e98f3d9d46c5c02d2c8ca9f23567c0c
jonatan almache montoya <levitax17@hotmail.com>
2020-10-30 11:49:29 -0500

    Código fuente

---
+++ ServicioMoneda/Models/ComprasDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace ServicioMoneda.Models
+{
+    class ComprasDbContext : DbContext
+    {
+            public ComprasDbContext()
+            {
+            }
+            public ComprasDbContext(DbContextOptions options)
+            : base(options)
+            {
+            }
+
+           
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                var configurationBuilder = new ConfigurationBuilder();
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+                configurationBuilder.AddJsonFile(path, false);
+
+            if (!optionsBuilder.IsConfigured)
+                {
+                    optionsBuilder.UseSqlServer(configurationBuilder.Build().GetSection("ConnectionStrings:bd").Value);
+                }
+            }
+
+            //Tablas de datos
+            public virtual DbSet<Compras> Compras { get; set; }
+            public virtual DbSet<Log> Log { get; set; }
+            public virtual DbSet<Usuarios> Usuarios { get; set; }
+    }
+}
---
+++ ServicioMoneda/Models/Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace ServicioMoneda.Models
+{
+    public class Log
+    {
+        [Key]
+        public string ID_LOG { get; set; }
+        public string Mensaje { get; set; }
+        public string Fecha { get; set; }
+
+    }
+}
